f71747ab4df37fe182b8f05f6cf723d3eef1c999
Shashi Kant <shashikantkaushik4@gmail.com>
2023-08-23 08:26:47 +0530

    provider

--- lib/screens/notifyListenerScreen.dart
+++ lib/screens/notifyListenerScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class NotifiyListenerScreen extends StatelessWidget {
    NotifiyListenerScreen({super.key});
    ValueNotifier<int> _counter=ValueNotifier<int>(0);
+   ValueNotifier<bool> toggle=ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +13,35 @@ class NotifiyListenerScreen extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body:Center(
-        child: ValueListenableBuilder(
-          valueListenable: _counter,
-          builder: (context,value,child){
-            return Text(_counter.value.toString(),style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.w900
-            ),);
-          }
-        ),
+        child: Column(children: [
+          ValueListenableBuilder(valueListenable: toggle,
+              builder: (context,value,child){
+            return TextFormField(
+              obscureText: toggle.value,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                suffix: InkWell(
+                  onTap: (){
+                    toggle.value=!toggle.value;
+                  },
+                  child: Icon(
+                    toggle.value? Icons.visibility_off_outlined : Icons.visibility
+                  ),
+                )
+              ),
+            );
+              }),
+          ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context,value,child){
+                return Text(_counter.value.toString(),style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.w900
+                ),);
+              }
+          )
+        ],)
+        ,
       ),
           floatingActionButton: FloatingActionButton(
         onPressed: (){
